bfdc3882fd5180c1c3dd5bcd3cd8190beb5f14ae
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-05-27 00:49:34 -0500

    add audio sound and get timer working

--- js/app.js
+++ js/app.js
@@ -2,8 +2,47 @@ let breakInput = document.getElementById("display-break")
 let timeInput = document.getElementById("display-time")
 const container = document.getElementById("container")
 // const value = 1
-function increase() {
+var seconds = 0;
+var restSeconds = 0
+var audio = new Audio('./sound/3.mp3');
+var interval;
+// var counter = 0
+function convertseconds(s) {
+    let min = Math.floor(s / 60)
+    let sec = s % 60
+    return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 2 }).format(min) + ":" + new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(sec)
+}
+function playAudio() {
 
+}
+// function pomodoro(mins) {
+//     seconds = mins * 60 || 0;
+
+//     interval = setInterval(function () {
+//         document.getElementById("test").innerHTML = convertseconds(seconds)
+//         seconds--;
+//         if (seconds === 0) {
+//             clearInterval(interval);
+//             // alert("🚨 It is Cool 😎. I wish you could share ");
+//             audio.play();
+//         }
+//     }, 10)
+// }
+function pomodoro(time, rest) {
+    seconds = time * 60 || 0;
+    restSeconds = rest * 60 || 0;
+    interval = setInterval(function () {
+        document.getElementById("test").innerHTML = convertseconds(seconds)
+        seconds--;
+        if (seconds === 0) {
+
+            // alert("🚨 It is Cool 😎. I wish you could share ");
+            audio.play();
+            restSeconds--
+            document.getElementById("test").innerHTML = convertseconds(restSeconds)
+            clearInterval(interval);
+        }
+    }, 10)
 }
 container.addEventListener("click", function (event) {
     // const value = 1
@@ -38,11 +77,7 @@ container.addEventListener("click", function (event) {
             timeInput.value = timeValue
         }
 
+    } else if (elementTag == "fas fa-play") {
+        pomodoro(timeInput.value, breakInput.value)
     }
-
-    console.log(timeInput.value)
 })
-var interval
-function pomodoro(mins) {
-
-}
