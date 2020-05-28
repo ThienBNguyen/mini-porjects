643b9ed86088aaff5697f651345c9f7cfff73f64
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-05-27 23:23:33 -0500

    finish the play and the clear button but the pause button work but can not replay as the stop time

--- js/app.js
+++ js/app.js
@@ -1,63 +1,57 @@
 let breakInput = document.getElementById("display-break")
 let timeInput = document.getElementById("display-time")
 const container = document.getElementById("container")
-// const value = 1
 var seconds = 0;
 var restSeconds = 0
 var audio = new Audio('./sound/3.mp3');
 var interval;
-// var counter = 0
+var breakInterval
 function convertseconds(s) {
     let min = Math.floor(s / 60)
     let sec = s % 60
     return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 2 }).format(min) + ":" + new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(sec)
 }
-function playAudio() {
 
+
+function breakTime(rest) {
+    restSeconds = rest * 60 || 0;
+
+    breakInterval = setInterval(function () {
+        document.getElementById("timer").innerHTML = convertseconds(restSeconds)
+        restSeconds--;
+        if (restSeconds === 0) {
+            clearInterval(breakInterval);
+            audio.play();
+            document.getElementById("timer").innerHTML = 00 + ":" + 00
+
+        }
+    }, 1000)
 }
-// function pomodoro(mins) {
-//     seconds = mins * 60 || 0;
-
-//     interval = setInterval(function () {
-//         document.getElementById("test").innerHTML = convertseconds(seconds)
-//         seconds--;
-//         if (seconds === 0) {
-//             clearInterval(interval);
-//             // alert("🚨 It is Cool 😎. I wish you could share ");
-//             audio.play();
-//         }
-//     }, 10)
-// }
 function pomodoro(time, rest) {
     seconds = time * 60 || 0;
-    restSeconds = rest * 60 || 0;
     interval = setInterval(function () {
-        document.getElementById("test").innerHTML = convertseconds(seconds)
+        document.getElementById("timer").innerHTML = convertseconds(seconds)
         seconds--;
         if (seconds === 0) {
 
-            // alert("🚨 It is Cool 😎. I wish you could share ");
             audio.play();
-            restSeconds--
-            document.getElementById("test").innerHTML = convertseconds(restSeconds)
+            breakTime(rest)
             clearInterval(interval);
+
         }
-    }, 10)
+    }, 1000)
 }
 container.addEventListener("click", function (event) {
-    // const value = 1
     const element = event.target
     const elementTag = element.className
-    // console.log(elementTag)
+    let breakTest = 0
+    let sessionTest = 0
     if (elementTag == "fas fa-arrow-up break") {
-        // value += 1
         breakInput.value = parseInt(breakInput.value) + 5
-        // console.log(value)
     }
     else if (elementTag == "fas fa-arrow-down break") {
 
         const breakValue = parseInt(breakInput.value) - 5
-        console.log(breakValue)
         if (breakValue < 0) {
             breakInput.value = 0
         }
@@ -78,6 +72,22 @@ container.addEventListener("click", function (event) {
         }
 
     } else if (elementTag == "fas fa-play") {
-        pomodoro(timeInput.value, breakInput.value)
+        if (timeInput.value == 0 || breakInput.value == 0) {
+            alert('Please add more time!!')
+            clearInterval(interval)
+            clearInterval(breakInterval)
+        } else {
+            pomodoro(timeInput.value, breakInput.value)
+
+        }
+    } else if (elementTag == "fas fa-pause") {
+
+        clearInterval(interval)
+        clearInterval(breakInterval)
+    }
+    else if (elementTag == "fas fa-sync-alt") {
+        clearInterval(interval)
+        clearInterval(breakInterval)
+        document.getElementById("timer").innerHTML = 00 + ":" + 00
     }
 })
